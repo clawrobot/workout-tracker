1881ac8e6f5d02cf25fc0e56b51169cbe8b96b9f
Evan Langford <langfordevana@gmail.com>
2025-08-26 21:42:35 -0500

    Setup react frontend

---
+++ apps/web/src/App.tsx
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <main className="min-h-screen grid place-items-center bg-gray-50">
+      <section className="p-8 rounded-2xl shadow bg-white">
+        <h1 className="text-3xl font-bold">Workout Tracker</h1>
+        <p className="mt-2 text-gray-600">Tailwind is working 🎉</p>
+      </section>
+    </main>
+  );
+}
+export default App;
